3abacbabd583fe2c2c3036e52e355c9a047fe853
seiryu_dev <laak.felix@gmail.com>
2026-02-16 02:30:09 +0100

    added dotenv library and .env.example file; now loading token from .env file;

--- Discord-bot.Infrastructure/Configuration/DiscordOptions.cs
+++ Discord-bot.Infrastructure/Configuration/DiscordOptions.cs
@@ -1,6 +1,15 @@
+using dotenv.net;
+
 namespace Discord_bot.infrastructure.Configuration;
 
 public class DiscordOptions
 {
-    public string Token  { get; } = "YOUR_TOKEN_HERE";
+    public readonly string Token;
+
+    public DiscordOptions()
+    {
+        DotEnv.Load();
+        var envVars = DotEnv.Read();
+        Token = envVars["DISCORD_TOKEN"];
+    }
 }
